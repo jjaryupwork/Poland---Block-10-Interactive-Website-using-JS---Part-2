118f25da2ec1485d764c35f6eaf8307e88299b88
jjaryupwork <jjaryupwork@gmail.com>
2017-06-07 11:43:30 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,126 +1,85 @@
+var currentArticle;
+var prevArticle; 
+var currentDot; 
+var prevDot; 
+var nextArticle;
+var nextDot;
+var switchCssClassesn = function(currentArticle, nextArticle, current){
+    currentArticle.removeClass('current');
+    nextArticle.addClass('current');
+   };
+var switchCssClassesndot = function(currentDot, nextDot, activedot){
+    currentDot.removeClass('activedot');
+    nextDot.addClass('activedot');
+    };
+var switchCssClassesp = function(currentArticle, prevArticle, current){
+    currentArticle.removeClass('current');
+    prevArticle.addClass('current');
+   };
+var switchCssClassespdot = function(currentDot, prevDot, activedot){
+currentDot.removeClass('activedot');
+    prevDot.addClass('activedot');
+    };
 var main = function() {
     $('.article').click(function() { 
     $('.article').removeClass('current');  
     $('.ppp').hide();
-    
-
     $(this).addClass('current');
     $(this)('.ppp').show();
   });
-    /* var nextArticle =currentArticle.next();
-     var currentDot = $('.active-dot');
-    var nextDot = currentDot.next();
-    var currentArticle = $('.current');
-    var prevArticle =currentArticle.prev();
-    var currentDot = $('.active-dot');
-    var prevDot = currentDot.prev();
-     var currentArticle = $('.current');*/
-   
-var zzz = function(){
-  /*var*/ currentArticle //= $('.current');
-     /*var*/ nextArticle// =currentArticle.next();
-    /* var*/ currentDot//= $('.active-dot');
-    /*var*/ nextDot //= currentDot.next();
-   
-};
-
-var yyy = function(){
-    /* var*/ currentArticle// = $('.current');
-    /*var */prevArticle// =currentArticle.prev();
-    /*var*/ currentDot// = $('.active-dot');
-   /* var*/ prevDot //= currentDot.prev();
-   
-};
-
-
-  
 $('.arrow-next').click(function(){  
-   // zzz(); 
-   var currentArticle = $('.current');
-     var nextArticle =currentArticle.next();
-     var currentDot = $('.active-dot');
-    var nextDot = currentDot.next();
+     currentArticle = $('.current');
+     nextArticle =currentArticle.next();
+     currentDot = $('.activedot');
+     nextDot = currentDot.next();
     if(nextArticle.length == 0){
-        nextArticle = $('.article').first();
-        nextDot = $('.dot').first();
+      nextArticle = $('.article').first();
+      nextDot = $('.dot').first();
         }
-  currentArticle.removeClass('current');
-  nextArticle.addClass('current');
-    
-  currentDot.removeClass('active-dot');
-  nextDot.addClass('active-dot');
-        
-   
-   
+    switchCssClassesn (currentArticle, nextArticle,0, 'current')
+    switchCssClassesndot (currentDot, nextDot,0, 'activedot')     
 });
 $('.arrow-prev').click(function() {
-   //yyy();
-     var currentArticle = $('.current');
-    var prevArticle =currentArticle.prev();
-    var currentDot = $('.active-dot');
-    var prevDot = currentDot.prev();
+     currentArticle = $('.current');
+     prevArticle =currentArticle.prev();
+     currentDot = $('.activedot');
+     prevDot = currentDot.prev();
     if(prevArticle.length == 0){
-        prevArticle = $('.article').last();
-        prevDot = $('.dot').last();
+      prevArticle = $('.article').last();
+      prevDot = $('.dot').last();
     }
-
-   currentArticle.removeClass('current');
-    prevArticle.addClass('current');
-    
-    currentDot.removeClass('active-dot');
-    prevDot.addClass('active-dot');
+    switchCssClassesp (currentArticle, prevArticle,0, 'current')
+    switchCssClassespdot (currentDot, prevDot,0, 'activedot')
   });
-
-
    $(document).keypress(function(event) {
      if(event.which === 111) {
     $('.current').children('.ppp').toggle();
   }
   else if(event.which === 110) {
-   //zzz(); 
-    var currentArticle = $('.current');
-    var nextArticle = currentArticle.next();  
-
-    var currentDot = $('.active-dot');
-    var nextDot = currentDot.next();
-     
+     currentArticle = $('.current');
+     nextArticle = currentArticle.next();  
+     currentDot = $('.activedot');
+     nextDot = currentDot.next();
    if(nextArticle.length == 0){
-        nextArticle = $('.article').first();
-        nextDot = $('.dot').first();
+     nextArticle = $('.article').first();
+     nextDot = $('.dot').first();
         }
-
-    currentArticle.removeClass('current');
-     nextArticle.addClass('current');
-
-    currentDot.removeClass('active-dot');
-    nextDot.addClass('active-dot');
-  
+    switchCssClassesn (currentArticle, nextArticle,0, 'current')
+    switchCssClassesndot (currentDot, nextDot,0, 'activedot')  
 }
   else if(event.which === 98) {
-  // yyy(); 
-    var currentArticle = $('.current');
-    var prevArticle = currentArticle.prev();
-
-    var currentDot = $('.active-dot');
-    var prevDot = currentDot.prev();
-     
-
+     currentArticle = $('.current');
+     prevArticle = currentArticle.prev();
+     currentDot = $('.activedot');
+     prevDot = currentDot.prev();
     if(prevArticle.length == 0){
-        prevArticle = $('.article').last();
-        prevDot = $('.dot').last();
+      prevArticle = $('.article').last();
+      prevDot = $('.dot').last();
     }
-
-
-     currentArticle.removeClass('current');
-    prevArticle.addClass('current');
-
-   currentDot.removeClass('active-dot');
-    prevDot.addClass('active-dot');
-  
+    switchCssClassesp (currentArticle, prevArticle,0, 'current')
+    switchCssClassespdot (currentDot, prevDot,0, 'activedot') 
 }
-
 });
 }
 $(document).ready(main);
-
 
