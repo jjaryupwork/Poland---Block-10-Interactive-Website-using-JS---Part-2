deb2e8d6ea1b2fb11a37c822a47bdf2b26d17ef0
jjaryupwork <jjaryupwork@gmail.com>
2017-06-07 20:38:32 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,85 +1,50 @@
-var currentArticle;
-var prevArticle; 
-var currentDot; 
-var prevDot; 
-var nextArticle;
-var nextDot;
-var switchCssClassesn = function(currentArticle, nextArticle, current){
-    currentArticle.removeClass('current');
-    nextArticle.addClass('current');
-   };
-var switchCssClassesndot = function(currentDot, nextDot, activedot){
-    currentDot.removeClass('activedot');
-    nextDot.addClass('activedot');
-    };
-var switchCssClassesp = function(currentArticle, prevArticle, current){
-    currentArticle.removeClass('current');
-    prevArticle.addClass('current');
-   };
-var switchCssClassespdot = function(currentDot, prevDot, activedot){
-currentDot.removeClass('activedot');
-    prevDot.addClass('activedot');
-    };
-var main = function() {
-    $('.article').click(function() { 
-    $('.article').removeClass('current');  
-    $('.ppp').hide();
-    $(this).addClass('current');
-    $(this)('.ppp').show();
-  });
-$('.arrow-next').click(function(){  
-     currentArticle = $('.current');
-     nextArticle =currentArticle.next();
-     currentDot = $('.activedot');
-     nextDot = currentDot.next();
-    if(nextArticle.length == 0){
-      nextArticle = $('.article').first();
-      nextDot = $('.dot').first();
-        }
-    switchCssClassesn (currentArticle, nextArticle,0, 'current')
-    switchCssClassesndot (currentDot, nextDot,0, 'activedot')     
-});
-$('.arrow-prev').click(function() {
-     currentArticle = $('.current');
-     prevArticle =currentArticle.prev();
-     currentDot = $('.activedot');
-     prevDot = currentDot.prev();
-    if(prevArticle.length == 0){
-      prevArticle = $('.article').last();
-      prevDot = $('.dot').last();
-    }
-    switchCssClassesp (currentArticle, prevArticle,0, 'current')
-    switchCssClassespdot (currentDot, prevDot,0, 'activedot')
-  });
-   $(document).keypress(function(event) {
-     if(event.which === 111) {
-    $('.current').children('.ppp').toggle();
+
+var checkbox = function(){
+ if ('geolocation' in navigator) {
+     setInterval(getWeather, 100000);
+     getWeather();
   }
-  else if(event.which === 110) {
-     currentArticle = $('.current');
-     nextArticle = currentArticle.next();  
-     currentDot = $('.activedot');
-     nextDot = currentDot.next();
-   if(nextArticle.length == 0){
-     nextArticle = $('.article').first();
-     nextDot = $('.dot').first();
-        }
-    switchCssClassesn (currentArticle, nextArticle,0, 'current')
-    switchCssClassesndot (currentDot, nextDot,0, 'activedot')  
+  else
+  {
+    alert('msg');
+    $('.error >p').text('Sorry u dont allow to get Weather');
+  }
 }
-  else if(event.which === 98) {
-     currentArticle = $('.current');
-     prevArticle = currentArticle.prev();
-     currentDot = $('.activedot');
-     prevDot = currentDot.prev();
-    if(prevArticle.length == 0){
-      prevArticle = $('.article').last();
-      prevDot = $('.dot').last();
+
+
+    var getWeather = function() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      loadWeather(position.coords.latitude + ',' + position.coords.longitude);
+    });
+  } 
+ 
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'c',
+    success: function (weather) {
+      city = weather.city;
+      temp = weather.temp + '&deg;';
+      wcode = '<img class="weathericon" src="images/' + weather.code + '.svg">';
+      wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
+      humidity = weather.humidity + '%';
+      $(".location").text(city);
+      $(".temperature").html(temp);
+      $(".climate_bg").html(wcode);
+      $(".windspeed").html(wind);
+      $(".humidity").text(humidity);
+    },
+	 error: function(error) {
+   $(".error").html('<p>' + error + '</p>');
     }
-    switchCssClassesp (currentArticle, prevArticle,0, 'current')
-    switchCssClassespdot (currentDot, prevDot,0, 'activedot') 
+  });
 }
+
+$(document).ready(function (checkbox) {
+ 
 });
-}
-$(document).ready(main);
+
+
+
 
