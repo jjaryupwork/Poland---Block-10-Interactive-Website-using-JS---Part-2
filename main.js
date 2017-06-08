0e0b20142b4f1d811e71ba5debeb1298a867802d
jjaryupwork <jjaryupwork@gmail.com>
2017-06-08 19:42:33 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,17 +4,10 @@ var currentDot;
 var prevDot; 
 var nextArticle;
 var nextDot;
-
-
-
-  
 var switchCssClasses = function(prevElement, nextElement, ClassName) {
   prevElement = prevElement.removeClass(ClassName);
   nextElement = nextElement.addClass(ClassName);
 } 
- 
-
-
 var main = function() {
     $('.article').click(function() { 
     $('.article').removeClass('current');  
@@ -30,11 +23,9 @@ $('.arrow-next').click(function(){
     if(nextArticle.length == 0){
       nextArticle = $('.article').first();
       nextDot = $('.dot').first();
-        }
-
+      }
     switchCssClasses( currentArticle, nextArticle,  'current' );
     switchCssClasses( currentDot, nextDot,  'activedot' );
-
 });
 $('.arrow-prev').click(function() {
      currentArticle = $('.current');
@@ -44,16 +35,14 @@ $('.arrow-prev').click(function() {
     if(prevArticle.length == 0){
       prevArticle = $('.article').last();
       prevDot = $('.dot').last();
-    }
+      }
     switchCssClasses( currentArticle, prevArticle,  'current' );
     switchCssClasses( currentDot, prevDot,  'activedot' );
-
-   
   });
    $(document).keypress(function(event) {
      if(event.which === 111) {
     $('.current').children('.ppp').toggle();
-  }
+      }
   else if(event.which === 110) {
      currentArticle = $('.current');
      nextArticle = currentArticle.next();  
@@ -62,10 +51,10 @@ $('.arrow-prev').click(function() {
    if(nextArticle.length == 0){
      nextArticle = $('.article').first();
      nextDot = $('.dot').first();
-        }
+      }
    switchCssClasses( currentArticle, nextArticle,  'current' );
     switchCssClasses( currentDot, nextDot,  'activedot' );  
-}
+      }
   else if(event.which === 98) {
      currentArticle = $('.current');
      prevArticle = currentArticle.prev();
@@ -74,11 +63,10 @@ $('.arrow-prev').click(function() {
     if(prevArticle.length == 0){
       prevArticle = $('.article').last();
       prevDot = $('.dot').last();
-    }
+       }
      switchCssClasses( currentArticle, prevArticle,  'current' );
     switchCssClasses( currentDot, prevDot,  'activedot' );
-    
-}
+      }
 });
 }
 $(document).ready(main);
